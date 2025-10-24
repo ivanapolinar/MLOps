da5f537587bdebda56b2470625c3cc7fd11ce160
Kailin Wu <77127850+kw1013@users.noreply.github.com>
2025-10-23 20:31:40 -0600

    POO refactoring for train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from typing import Type, Dict, Any, Optional, Union
+
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.model_selection import (
     train_test_split, RandomizedSearchCV
 )
@@ -14,196 +17,243 @@ from sklearn.metrics import (
     confusion_matrix, accuracy_score, classification_report
 )
 from sklearn.ensemble import RandomForestClassifier
+
 import joblib
 import mlflow
 import mlflow.sklearn
 
 
-def load_data(input_path):
-    df = pd.read_csv(input_path)
-    return df
 
 
-def split_data(df, target="Load_Type", test_size=0.2, random_state=42):
-    y = df[target]
-    X = df.drop(columns=[target, 'date'], errors="ignore")
-    return train_test_split(
-        X,
-        y,
-        test_size=test_size,
-        random_state=random_state,
-        stratify=y
-    )
+# ==================== Helper Classes ==================== #
+
+# Class representing the data repository, in this case, the data file
+class DataRepository:
+    def load(self, input_path: str) -> pd.DataFrame:
+        return pd.read_csv(input_path)
+
+# Class helping the data splitting based on a target variable, default values represent our current experiment
+class DataSplitter:
+    def __init__(self, target: str = 'Load_Type', cols_to_drop: list[str] = ['date'], test_size: float = 0.2, random_state: int = 42):
+        self.target = target
+        self.test_size = test_size
+        self.random_state = random_state
+        self.cols_to_drop = cols_to_drop
+
+    def split(self, df: pd.DataFrame) -> list:
+        y = df[self.target]
+        X = df.drop(columns=[self.target] + self.cols_to_drop)
+        return train_test_split(
+            X, y,
+            test_size=self.test_size,
+            random_state=self.random_state,
+            stratify=y
+        )
+
+# Class that helps making the preprocessing pipeline
+class PreprocessingBuilder:
+    def build(self, X: pd.DataFrame):
+        cat_cols = list(X.select_dtypes('object').columns)
+        num_cols = list(X.select_dtypes('number').columns)
+        preprocessing = ColumnTransformer(
+            transformers=[
+                ('num', StandardScaler(), num_cols),
+                ('cat', OneHotEncoder(), cat_cols)
+            ]
+        )
+        return preprocessing, cat_cols, num_cols
+
+# Class that handles model training
+class ModelTrainer:
+    def __init__(self, random_state: int = 42, params: dict | None = None):
+        self.random_state = random_state
+        self.params = params
+
+    def build_pipeline(self, preprocessing: ColumnTransformer) -> Pipeline:
+        rf = RandomForestClassifier(random_state=self.random_state, **self.params)
+        return make_pipeline(preprocessing, rf)
+
+    def fit(self, X_train: pd.DataFrame, y_train: pd.Series, preprocessing: ColumnTransformer) -> Pipeline:
+        model = self.build_pipeline(preprocessing)
+        model.fit(X_train, y_train)
+        return model
+
+# Class that handles evaluation and image generation
+class ModelEvaluator:
+    def evaluate(self, model: BaseEstimator, X_test: pd.DataFrame, y_test: pd.Series, figures_dir: str, name: str = 'base') -> dict:
+        y_pred = model.predict(X_test)
+        report = classification_report(y_test, y_pred)
+        acc = accuracy_score(y_test, y_pred)
+
+        print(f"Classification Report ({name}):")
+        print(classification_report(y_test, y_pred))
+        print("Accuracy:", acc)
+
+        plt.figure(figsize=(6, 4))
+        sns.heatmap(
+            confusion_matrix(y_test, y_pred),
+            annot=True,
+            fmt="d",
+            cmap="Blues"
+        )
+        plt.title(f"Confusion Matrix - RandomForest ({name})")
+        plt.xlabel("Predictions")
+        plt.ylabel("Real")
+        plt.tight_layout()
+        fig_path = f"{figures_dir}/confusion_matrix_{name}.png"
+        plt.savefig(fig_path)
+        plt.close()
+
+        return acc, fig_path, report
+
+# Class that handles hyperparameter tunning
+class HyperparameterTuner:
+    def __init__(self, param_dist: dict | None = None):
+        self.param_dist = param_dist if param_dist is not None else {
+            "randomforestclassifier__n_estimators": [100, 200, 400],
+            "randomforestclassifier__max_depth": [10, 20, None],
+            "randomforestclassifier__min_samples_split": [2, 5, 10],
+            "randomforestclassifier__min_samples_leaf": [1, 2, 4],
+            "randomforestclassifier__max_features": ["sqrt", "log2"]
+        }
+
+    def tune(self, model: Pipeline, X_train: pd.DataFrame, y_train: pd.Series, random_state: int = 42):
+        rf_rscv = RandomizedSearchCV(
+            estimator=model,
+            param_distributions=self.param_dist,
+            n_iter=30,
+            cv=3,
+            scoring='accuracy',
+            verbose=1,
+            n_jobs=-1,
+            random_state=42
+        )
+        rf_rscv.fit(X_train, y_train)
+        print("Best Hyperparameters:")
+        print(rf_rscv.best_params_)
+
+        return rf_rscv.best_estimator_, rf_rscv.best_params_
+
+# Class that exports feature importances and graphs them
+class FeatureImportanceExporter:
+    def export(self, model: Pipeline, num_cols: list, cat_cols: list, figures_dir: str):
+        rf_final: RandomForestClassifier = model.named_steps["randomforestclassifier"]
+        ct: ColumnTransformer = model.named_steps["columntransformer"]
+        feature_names = ct.get_feature_names_out()
+
+        importances = rf_final.feature_importances_
+        feature_importances = pd.DataFrame({
+            "Feature": feature_names,
+            "Importance": importances
+        }).sort_values("Importance", ascending=False)
+
+        fi_path = f"{figures_dir}/feature_importances.csv"
+        feature_importances.head(15).to_csv(fi_path, index=False)
+
+        plt.figure(figsize=(10, 6))
+        sns.barplot(data=feature_importances.head(15), x="Importance", y="Feature")
+        plt.title("Top 15 Most Important Variables - RandomForest")
+        plt.xlabel("Importance")
+        plt.ylabel("Variable")
+        plt.tight_layout()
+        top_feat_path = f"{figures_dir}/top_features.png"
+        plt.savefig(top_feat_path)
+        plt.close()
+
+        return fi_path, top_feat_path
+
+class ModelPersister():
+    def save(self, model: Pipeline, model_path: str):
+        joblib.dump(model, model_path)
+        print(f"Model saved in {model_path}")
 
 
-def build_preprocessing(X):
-    cat_cols = list(X.select_dtypes('object').columns)
-    num_cols = list(X.select_dtypes('number').columns)
-    preprocessing = ColumnTransformer(
-        transformers=[
-            ('num', StandardScaler(), num_cols),
-            ('cat', OneHotEncoder(handle_unknown='ignore'), cat_cols)
-        ]
-    )
-    return preprocessing, num_cols, cat_cols
 
 
-def train_base_model(X_train, y_train, preprocessing, params=None):
-    rf = RandomForestClassifier(random_state=42, **(params or {}))
-    rf_model = make_pipeline(preprocessing, rf)
-    rf_model.fit(X_train, y_train)
-    return rf_model
+# ==================== Orchestrator Class ==================== #
+
+# Orchestrates all preprocessing, training, evaluation, and logging for model processing
+class ExperimentRunner():
+    def __init__(self):
+        self.data_repo = DataRepository()
+        self.splitter = DataSplitter()
+        self.pre_builder = PreprocessingBuilder()
+        self.trainer = ModelTrainer()
+        self.evaluator = ModelEvaluator()
+        self.tuner = HyperparameterTuner()
+        self.fi_exporter = FeatureImportanceExporter()
+        self.persister = ModelPersister()
+
+    def run(self, input_path: str, model_path: str, figures_dir: str):
+        # Make dirs for outputs
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+        os.makedirs(figures_dir, exist_ok=True)
+
+        # Read Data
+        df = self.data_repo.load(input_path)
+
+        # Split data
+        X_train, X_test, y_train, y_test = self.splitter.split(df)
+
+        # Preprocessing pipeline and feature cols
+        preprocessing, num_cols, cat_cols = self.pre_builder.build(X_train)
+        
+        # Start run with mlflow
+        mlflow.set_experiment("rf-experiment")
+        with mlflow.start_run():
+            # Base training
+            base_params = {}
+            base_trainer = ModelTrainer(params=base_params)
+            rf_model = base_trainer.fit(X_train, y_train, preprocessing)
+            mlflow.log_params(base_params)
+
+            # Base Metric logging
+            acc_base, fig_base, report_base = self.evaluator.evaluate(rf_model, X_test, y_test, figures_dir, name='base')
+            mlflow.log_metric('base_accuracy', acc_base)
+            mlflow.log_artifact(fig_base)
+
+            # Hyperparameter tunning
+            best_model, best_params = self.tuner.tune(rf_model, X_train, y_train)
+            mlflow.log_params(best_params)
+
+            # Best Metric Logging
+            acc_opt, fig_opt, report_opt = self.evaluator.evaluate(best_model, X_test, y_test, figures_dir, name='optimized')
+            mlflow.log_metric('optimized_accuracy', acc_opt)
+            mlflow.log_artifact(fig_opt)
+
+            # Calculate feature importances
+            fi_path, top_feat_path = self.fi_exporter.export(best_model, num_cols, cat_cols, figures_dir)
+            mlflow.log_artifact(fi_path)
+            mlflow.log_artifact(top_feat_path)
+
+            # Persist model
+            self.persister.save(best_model, model_path)
+
+            # Log example model
+            X_example = X_test[:2]
+            mlflow.sklearn.log_model(
+                best_model,
+                name='model',
+                input_example=X_example,
+                signature=mlflow.models.infer_signature(
+                    X_example, best_model.predict(X_example)
+                )
+            )
 
 
-def evaluate_model(model, X_test, y_test, figures_dir, name="base"):
-    y_pred = model.predict(X_test)
-    report = classification_report(y_test, y_pred, output_dict=True)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Reporte de clasificación ({name}):")
-    print(classification_report(y_test, y_pred))
-    print("Accuracy:", acc)
-    plt.figure(figsize=(6, 4))
-    sns.heatmap(
-        confusion_matrix(y_test, y_pred),
-        annot=True,
-        fmt="d",
-        cmap="Blues"
-    )
-    plt.title(f"Matriz de confusión - RandomForest ({name})")
-    plt.xlabel("Predicción")
-    plt.ylabel("Real")
-    plt.tight_layout()
-    fig_path = f"{figures_dir}/confusion_matrix_{name}.png"
-    plt.savefig(fig_path)
-    plt.close()
-    return acc, fig_path, report
 
 
-def hyperparameter_tuning(model, X_train, y_train):
-    param_dist = {
-        "randomforestclassifier__n_estimators": [100, 200, 400],
-        "randomforestclassifier__max_depth": [10, 20, None],
-        "randomforestclassifier__min_samples_split": [2, 5, 10],
-        "randomforestclassifier__min_samples_leaf": [1, 2, 4],
-        "randomforestclassifier__max_features": ["sqrt", "log2"]
-    }
-    rf_random = RandomizedSearchCV(
-        estimator=model,
-        param_distributions=param_dist,
-        n_iter=30,
-        cv=3,
-        scoring='accuracy',
-        verbose=1,
-        n_jobs=-1,
-        random_state=42
-    )
-    rf_random.fit(X_train, y_train)
-    print("Mejores hiperparámetros encontrados:")
-    print(rf_random.best_params_)
-    return rf_random.best_estimator_, rf_random.best_params_
+# ==================== CLI ==================== #
 
-
-def save_feature_importance(model, num_cols, cat_cols, figures_dir):
-    rf_final = model.named_steps["randomforestclassifier"]
-    ohe = model.named_steps["columntransformer"].named_transformers_["cat"]
-    encoded_cat_cols = ohe.get_feature_names_out(cat_cols)
-    final_feature_names = np.concatenate([num_cols, encoded_cat_cols])
-    importances = rf_final.feature_importances_
-    feature_importances = pd.DataFrame({
-        "Feature": final_feature_names,
-        "Importance": importances
-    }).sort_values("Importance", ascending=False)
-    fi_path = f"{figures_dir}/feature_importances.csv"
-    feature_importances.head(15).to_csv(fi_path, index=False)
-    plt.figure(figsize=(10, 6))
-    sns.barplot(data=feature_importances.head(15), x="Importance", y="Feature")
-    plt.title("Top 15 Variables más importantes - RandomForest")
-    plt.xlabel("Importancia")
-    plt.ylabel("Variable")
-    plt.tight_layout()
-    top_feat_path = f"{figures_dir}/top_features.png"
-    plt.savefig(top_feat_path)
-    plt.close()
-    return fi_path, top_feat_path
-
-
-def save_model(model, model_path):
-    joblib.dump(model, model_path)
-    print(f"Modelo guardado en {model_path}")
-
-
+# Orchestrates all preprocessing, training, evaluation, and logging for model processing
 @click.command()
 @click.argument('input_path', type=click.Path(exists=True))
 @click.argument('model_path', type=click.Path())
 @click.argument('figures_dir', type=click.Path())
 def main(input_path, model_path, figures_dir):
-    """Entrena y guarda el modelo Random Forest,
-    y exporta resultados gráficos."""
-    os.makedirs(os.path.dirname(model_path), exist_ok=True)
-    os.makedirs(figures_dir, exist_ok=True)
-    df = load_data(input_path)
-    X_train, X_test, y_train, y_test = split_data(df)
-    preprocessing, num_cols, cat_cols = build_preprocessing(X_train)
-
-    # MLflow tracking
-    with mlflow.start_run():
-        # Entrenamiento base y log de parámetros
-        base_params = {}
-        rf_model = train_base_model(
-            X_train, y_train,
-            preprocessing,
-            base_params
-        )
-        mlflow.log_params(base_params)
-        acc_base, fig_base, report_base = evaluate_model(
-            rf_model,
-            X_test,
-            y_test,
-            figures_dir,
-            name="base"
-        )
-        mlflow.log_metric("base_accuracy", acc_base)
-        mlflow.log_artifact(fig_base)
-
-        # Hiperparámetros
-        best_model, best_params = hyperparameter_tuning(
-            rf_model,
-            X_train,
-            y_train
-        )
-        mlflow.log_params(best_params)
-        acc_opt, fig_opt, report_opt = evaluate_model(
-            best_model,
-            X_test,
-            y_test,
-            figures_dir,
-            name="optimized"
-        )
-        mlflow.log_metric("optimized_accuracy", acc_opt)
-        mlflow.log_artifact(fig_opt)
-
-        # Importancia de variables
-        fi_path, top_feat_path = save_feature_importance(
-            best_model,
-            num_cols,
-            cat_cols,
-            figures_dir
-        )
-        mlflow.log_artifact(fi_path)
-        mlflow.log_artifact(top_feat_path)
-
-        # Guardar modelo local y en MLflow
-        save_model(best_model, model_path)
-        X_example = X_test[:2]
-        mlflow.sklearn.log_model(
-            best_model,
-            name="model",
-            input_example=X_example,
-            signature=mlflow.models.infer_signature(
-                X_example,
-                best_model.predict(X_example)
-            )
-        )
+    """Entrena y guarda el modelo Random Forest, y exporta resultados gráficos."""
+    runner = ExperimentRunner()
+    runner.run(input_path, model_path, figures_dir)
 
 
 if __name__ == "__main__":
