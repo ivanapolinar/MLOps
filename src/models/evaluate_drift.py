327346287af29c6908d00df71699dc3b11f8d293
Kailin Wu <77127850+kw1013@users.noreply.github.com>
2025-11-13 20:03:47 -0600

    feat: added images for kde plots for distribution observation

--- src/models/evaluate_drift.py
+++ src/models/evaluate_drift.py
@@ -1,17 +1,50 @@
 import click
 import pandas as pd
+import seaborn as sns
 from alibi_detect.cd import KSDrift
+import matplotlib.pyplot as plt
 from src.models.train_model import (
     DataRepository,
-    DataSplitter,
-    PreprocessingBuilder
+    DataSplitter
 )
+
+
+def create_kde_plot(X_train: pd.DataFrame, X_eval: pd.DataFrame, feature: str, figures_dir: str):
+    """
+    Inputs:
+        - X_train: DataFrame with train data
+        - synthetic_data: Synthetic train data
+        - feature: Column to analyze
+        - figures_dir: Path to store figures
+    Purpose:
+        - Elaborate a kde plot to observe distribution drift
+    """
+    plt.figure(figsize=(10,5))
+
+    sns.kdeplot(X_train[feature], label='Original Data', fill=True, color='blue')
+
+    sns.kdeplot(X_eval[feature], label='Eval Data', fill=True, color='red')
+
+    plt.title('Comparison Original vs Evaluation', fontsize=16)
+    plt.xlabel(feature, fontsize=14)
+    plt.ylabel('Density', fontsize=14)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'{figures_dir}/kde_plot_evaluate_{feature}.png')
+    plt.close()
 
 
 @click.command()
 @click.argument("train_data_path", type=click.Path(exists=True))
 @click.argument("eval_data_path", type=click.Path())
-def main(train_data_path: str, eval_data_path: str) -> bool:
+@click.argument("feature", type=click.Path())
+@click.argument("figures_dir", type=click.Path())
+def main(
+    train_data_path: str,
+    eval_data_path: str,
+    feature: str,
+    figures_dir: str
+):
     """
     Inputs:
         - train_data_path: Train dataset path
@@ -25,7 +58,7 @@ def main(train_data_path: str, eval_data_path: str) -> bool:
     eval = DataRepository().load(eval_data_path)
     print(
         f"""\nTrain ({train_data_path})
-        and Eval ({eval_data_path}) data loaded\n"""
+and Eval ({eval_data_path}) data loaded\n"""
     )
 
     # Split data accordingly
@@ -33,31 +66,18 @@ def main(train_data_path: str, eval_data_path: str) -> bool:
         DataSplitter().split(train)
     print(f'Train ({train_data_path}) data splitted\n')
 
-    # Preprocess data as in training phase
-    pipeline, cat_cols, num_cols = PreprocessingBuilder().build(X_train)
-    X_train_proc = pd.DataFrame(
-        pipeline.fit_transform(X_train),
-        columns=pipeline.get_feature_names_out()
-    )
-    print(f'Train ({train_data_path}) data preprocessed\n')
-
     # Split test data into X and y
     X_eval = eval[X_train.columns]
 
-    # Preprocess the eval dataset as with the training data
-    X_eval_proc = pd.DataFrame(
-        pipeline.transform(X_eval),
-        columns=pipeline.get_feature_names_out()
-    )
-    print(f'Eval ({eval_data_path}) data preproccesed\n')
-
     # Train KSDrift and infer on eval data
     print('Calculating KSDrift\n')
-    cd = KSDrift(X_train_proc.values, p_val=0.05)
-    drift_pred = cd.predict(X_eval_proc.values)
+    cd = KSDrift(X_train.values, p_val=0.05)
+    drift_pred = cd.predict(X_eval.values)
 
     print('SK-Drifted detected: ', bool(drift_pred['data']['is_drift']), '\n')
 
+    create_kde_plot(X_train, X_eval, feature, figures_dir)
+    print('kde plot generated')
 
 if __name__ == "__main__":
     main()
--- src/models/simulate_drift.py
+++ src/models/simulate_drift.py
@@ -1,12 +1,38 @@
 import click
 import pandas as pd
+import seaborn as sns
 from alibi_detect.cd import KSDrift
 from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
 from src.models.train_model import (
     DataRepository,
-    DataSplitter,
-    PreprocessingBuilder
+    DataSplitter
 )
+
+
+def create_kde_plot(X_train: pd.DataFrame, synthetic_data: pd.DataFrame, feature: str, figures_dir: str):
+    """
+    Inputs:
+        - X_train: DataFrame with train data
+        - synthetic_data: Synthetic train data
+        - feature: Column to analyze
+        - figures_dir: Path to store figures
+    Purpose:
+        - Elaborate a kde plot to observe distribution drift
+    """
+    plt.figure(figsize=(10,5))
+
+    sns.kdeplot(X_train[feature], label='Original Data', fill=True, color='blue')
+
+    sns.kdeplot(synthetic_data[feature], label='Shifted Data', fill=True, color='red')
+
+    plt.title('Comparison Original vs Drifted', fontsize=16)
+    plt.xlabel(feature, fontsize=14)
+    plt.ylabel('Density', fontsize=14)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'{figures_dir}/kde_plot_simulate_{feature}.png')
+    plt.close()
 
 
 @click.command()
@@ -14,11 +40,13 @@ from src.models.train_model import (
 @click.argument("n_components", type=click.INT)
 @click.argument("feature", type=click.STRING)
 @click.argument("meanshift", type=click.FLOAT)
+@click.argument("figures_dir", type=click.Path())
 def main(
     train_data_path: str,
     n_components: int,
     feature: str,
-    meanshift: float
+    meanshift: float,
+    figures_dir: str
 ):
     """
     Inputs:
@@ -39,20 +67,12 @@ def main(
         DataSplitter().split(train)
     print(f'Train ({train_data_path}) data splitted\n')
 
-    # Preprocess data as in training phase
-    pipeline, cat_cols, num_cols = PreprocessingBuilder().build(X_train)
-    X_train_proc = pd.DataFrame(
-        pipeline.fit_transform(X_train),
-        columns=pipeline.get_feature_names_out()
-    )
-    print(f'Train ({train_data_path}) data preprocessed\n')
-
     # Split test data into X and y
     gmm = GaussianMixture(
         n_components=n_components,
         random_state=42
     ).fit(
-        X_train_proc[feature].to_numpy().reshape(-1, 1)
+        X_train[feature].to_numpy().reshape(-1, 1)
     )
     print('\nOriginal Means: \n', gmm.means_)
 
@@ -66,18 +86,25 @@ def main(
     print('\nModified Means: \n', mod_means)
 
     # Generate synthetic data with shifted means
-    synthetic_data = X_train_proc.copy()
+    synthetic_data = X_train.copy()
+
+    # Save synthetic data set
+    synthetic_data.to_csv('data/clean/steel_energy_synthetic.csv')
+
     synthetic_data[feature] = gmm.sample(
-        int(X_train_proc.describe()[feature]['count'])
+        int(X_train.describe()[feature]['count'])
     )[0].reshape(-1)
-    X_synthetic = synthetic_data[X_train_proc.columns].values
+    X_synthetic = synthetic_data[X_train.columns].values
 
     # Train KSDrift and infer on eval data
     print('Calculating KSDrift\n')
-    cd = KSDrift(X_train_proc.values, p_val=0.05)
+    cd = KSDrift(X_train.values, p_val=0.05)
     drift_pred = cd.predict(X_synthetic)
 
     print('SK-Drifted detected: ', bool(drift_pred['data']['is_drift']), '\n')
+
+    create_kde_plot(X_train, synthetic_data, feature, figures_dir)
+    print('kde plot generated')
 
 
 if __name__ == "__main__":
